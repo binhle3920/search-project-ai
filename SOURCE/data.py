5e78a7dc6b4eba38a7427b1907a181b9c1473f4f
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-07-24 10:56:39 +0700

    Add file data.py with function's description

---
+++ SOURCE/data.py
@@ -0,0 +1,17 @@
+import os
+
+"""
+The function get maze will return a 2D array, maze size (m x n) and pos of pacman (x, y)
+This is a coordinate and each element hold a value, present it is empty (0), wall (1), monster (3) or food (@)
+Ex maze:
+    0 0 1 0
+    0 0 1 2
+    0 0 3 0
+    0 0 0 0
+--> maze[2][2] will be 3
+"""
+
+def get_maze():
+    pass
+
+    
