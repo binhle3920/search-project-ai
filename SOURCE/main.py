63dde9fe14927a0c394aa6c66ac267153042298a
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-08-07 11:07:43 +0700

    Print more information (score and exe time) git push origin master

--- SOURCE/main.py
+++ SOURCE/main.py
@@ -1,5 +1,6 @@
 import graphic, data, level12_astar, level12_bfs
 import tkinter as tk
+import time
 
 def get_maze_path():
     string = input("Input maze name: ")
@@ -9,16 +10,22 @@ def get_maze_path():
 if __name__ == '__main__':
     image_path = get_maze_path()
     level = int(input("Input level of game (1/2/3/4): "))
-
+    
     root = tk.Tk()
     pacman_game = graphic.PacmanGame(image_path, master=root)
     if level == 1 or level == 2:
-        path = level12_astar.A_star_run("../INPUT/map2_lv1.txt")
+        start = time.time()
+        path = level12_astar.A_star_run(image_path)
+        end = time.time()
+        execution_time = end - start
 
-    pacman_game.master.title("Pacman Game")
-    pacman_game.draw_maze()
-    pacman_game.draw_pacman()
-
-
-    pacman_game.pacman_move(path, 1)
-    pacman_game.mainloop()
+    if not isinstance(path, bool):
+        pacman_game.master.title("Pacman Game")
+        pacman_game.draw_maze()
+        pacman_game.draw_pacman()
+        score = pacman_game.pacman_move(path, 1)
+        print("Score is:", score)
+        print("Execution time is: ", execution_time)
+        pacman_game.mainloop()
+    else:
+        print('Cant find any path to get to the food')
