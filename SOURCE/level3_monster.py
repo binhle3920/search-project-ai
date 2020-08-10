d4a3a581977f53573fc15096b8c7280d53a98b09
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-08-10 11:23:37 +0700

    Make monster move randomly

---
+++ SOURCE/level3_monster.py
@@ -0,0 +1,75 @@
+import random
+
+monster_init_pos = [] 
+monster_cur_pos = []
+monster_possible_pos = []
+
+def get_monster_init_pos(maze, size):
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if maze[i][j] == 3:
+                monster_init_pos.append((i, j))
+
+def monster_possible_move(maze, size):
+    a_monster_move = []
+    for monster in monster_init_pos:
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if monster[0] + i >= 0 and monster[0] + i < size[0] and monster[1] + j >= 0 and  monster[1] + j < size[1]:
+                    if maze[monster[0] + i][monster[1] + j] == 0:
+                        a_monster_move.append((monster[0] + i, monster[1] + j))
+        monster_possible_pos.append(a_monster_move)
+        a_monster_move = []
+
+def possible_choice(monster_number, direction):
+    if direction == "up":
+        pos = (monster_cur_pos[monster_number][0] + 1, monster_cur_pos[monster_number][1])
+        if pos in monster_possible_pos[monster_number]:
+            return pos
+        else:
+            return "up"
+    elif direction == "down":
+        pos = (monster_cur_pos[monster_number][0] - 1, monster_cur_pos[monster_number][1])
+        if pos in monster_possible_pos[monster_number]:
+            return pos
+        else:
+            return "down"
+    elif direction == "left":
+        pos = (monster_cur_pos[monster_number][0], monster_cur_pos[monster_number][1] - 1)
+        if pos in monster_possible_pos[monster_number]:
+            return pos
+        else:
+            return "left"
+    elif direction == "right":
+        pos = (monster_cur_pos[monster_number][0], monster_cur_pos[monster_number][1] + 1)
+        if pos in monster_possible_pos[monster_number]:
+            return pos
+        else:
+            return "right"
+
+monster_cur_pos = monster_init_pos
+def init_monster(maze, size):
+    get_monster_init_pos(maze, size)
+    monster_possible_move(maze, size)
+    
+    monster_path = []
+    for i in range(len(monster_init_pos)):
+        monster_path.append([monster_init_pos[i]])
+    return monster_path
+
+def monster_move(maze, monster_path):
+    for monster_number in range(len(monster_init_pos)):
+        available_direction = ["up", "down", "left", "right"]
+        while len(available_direction) > 0:
+            direction = random.choice(available_direction)
+            pos = possible_choice(monster_number, direction)
+            if isinstance(pos, tuple):
+                monster_path[monster_number].append(pos)
+                break
+            else:
+                available_direction.pop(available_direction.index(pos))
+        if len(available_direction) == 0:
+            monster_path[monster_number].append(monster_cur_pos[monster_number])
+    
+    return monster_path
+        
