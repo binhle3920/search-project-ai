c92829d6b40837ab519dc50f13a64389dd16d5b8
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-08-11 13:59:12 +0700

    Test level3

--- SOURCE/main.py
+++ SOURCE/main.py
@@ -16,25 +16,26 @@ def get_maze_path():
 
 if __name__ == '__main__':
     image_path = get_maze_path()
-    #level = int(input("Input level of game (1/2/3/4): "))
-    level=3
+    level = int(input("Input level of game (1/2/3/4): "))
+    speed = int(input("Input game speed (ms): "))
+
     root = tk.Tk()
     pacman_game = graphic.PacmanGame(image_path, master=root)
     if level == 1 or level == 2:
         start = time.time()
-        #path = level12_bfs.BFS(image_path)
-        path = level12_astar.A_star_run(image_path)
+        pacman_path = level12_astar.A_star_run(image_path)
+        monster_path = []
         end = time.time()
         execution_time = end - start
 
     if level == 3:
-        pacman_path, monster_path=lvl3.level3(image_path)
+        pacman_path, monster_path = lvl3.level3(image_path)
 
-    if not isinstance(path, bool):
+    if not isinstance(pacman_path, bool):
         pacman_game.master.title("Pacman Game")
         pacman_game.draw_maze()
         pacman_game.draw_pacman()
-        score = pacman_game.pacman_move(path, 1)
+        score = pacman_game.pacman_move(pacman_path, monster_path, 1, speed, 1)
         print("Score is:", score)
         print("Execution time is: ", execution_time)
         pacman_game.mainloop()
